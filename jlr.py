8a23ba73e0007f32270fb55e6cb3edccce748b5c
Joosep Pata <joosep.pata@gmail.com>
2018-05-23 16:34:42 +0200

    better tboard path

--- jlr.py
+++ jlr.py
@@ -14,7 +14,7 @@ from keras import losses
 from keras import backend as K
 from matplotlib.colors import LogNorm
 
-batch_size = 10000
+batch_size = 1000
 log_r_clip_value = 10.0
 
 parser = argparse.ArgumentParser()
@@ -227,9 +227,17 @@ logging_callback = keras.callbacks.LambdaCallback(
     on_epoch_end=lambda epoch, logs: logging.info("epoch_end {0} {1} {2}".format(epoch, logs["loss"], logs["val_loss"])) 
 )
 
-tb = keras.callbacks.TensorBoard(log_dir='./tblogs/{0}'.format(name), histogram_freq=10, write_grads=True, batch_size=batch_size)
+tb = keras.callbacks.TensorBoard(log_dir='./{0}/tb'.format(name), histogram_freq=10, write_grads=True, batch_size=batch_size)
 es = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=args.earlystop, verbose=0, mode='auto')
 ret = mod.fit(X_train, y_train, sample_weight=w_train, batch_size=batch_size, validation_data=(X_test, y_test, w_test), epochs=args.epochs, callbacks=[es, logging_callback, tb], verbose=1)
+
+plt.figure()
+plt.plot(ret.history["loss"][5:])
+plt.savefig("{0}/loss_train.pdf".format(name))
+
+plt.figure()
+plt.plot(ret.history["val_loss"][5:])
+plt.savefig("{0}/loss_test.pdf".format(name))
 
 plt.figure()
 plt.plot(ret.history["loss"][5:])
