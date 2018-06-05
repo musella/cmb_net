41eabf44101734ee333651f9d05243cb730e0d03
Joosep Pata <joosep.pata@gmail.com>
2018-06-05 09:18:59 +0200

    better data prep

--- data_prep.py
+++ data_prep.py
@@ -28,14 +28,24 @@ args = parser.parse_args()
 
 Xs = []
 ys = []
-for fn in glob.glob("samples/combined/*.csv".format(args.type))[:args.maxfiles]:
+
+reco_cols = ['num_leptons', 'leptons_pt_0', 'leptons_pt_1', 'leptons_eta_0', 'leptons_eta_1', 'leptons_phi_0', 'leptons_phi_1', 'leptons_mass_0', 'leptons_mass_1', 'num_jets', 'jets_pt_0', 'jets_pt_1', 'jets_pt_2', 'jets_pt_3', 'jets_pt_4', 'jets_pt_5', 'jets_pt_6', 'jets_pt_7', 'jets_pt_8', 'jets_pt_9', 'jets_eta_0', 'jets_eta_1', 'jets_eta_2', 'jets_eta_3', 'jets_eta_4', 'jets_eta_5', 'jets_eta_6', 'jets_eta_7', 'jets_eta_8', 'jets_eta_9', 'jets_phi_0', 'jets_phi_1', 'jets_phi_2', 'jets_phi_3',
+'jets_phi_4', 'jets_phi_5', 'jets_phi_6', 'jets_phi_7', 'jets_phi_8', 'jets_phi_9', 'jets_mass_0', 'jets_mass_1', 'jets_mass_2', 'jets_mass_3', 'jets_mass_4', 'jets_mass_5', 'jets_mass_6', 'jets_mass_7', 'jets_mass_8', 'jets_mass_9', 'jets_btagDeepCSV_0', 'jets_btagDeepCSV_1', 'jets_btagDeepCSV_2', 'jets_btagDeepCSV_3', 'jets_btagDeepCSV_4', 'jets_btagDeepCSV_5', 'jets_btagDeepCSV_6', 'jets_btagDeepCSV_7', 'jets_btagDeepCSV_8', 'jets_btagDeepCSV_9', 'met_pt', 'met_phi', 'met_sumEt',
+'nBDeepCSVM', 'mbb_closest', 'ht30']
+gen_cols = ['gen_num_leptons', 'gen_leptons_pt_0', 'gen_leptons_pt_1', 'gen_leptons_eta_0', 'gen_leptons_eta_1', 'gen_leptons_phi_0', 'gen_leptons_phi_1', 'gen_leptons_mass_0', 'gen_leptons_mass_1', 'gen_num_jets', 'gen_jets_pt_0',
+'gen_jets_pt_1', 'gen_jets_pt_2', 'gen_jets_pt_3', 'gen_jets_pt_4', 'gen_jets_pt_5', 'gen_jets_pt_6', 'gen_jets_pt_7', 'gen_jets_pt_8', 'gen_jets_pt_9', 'gen_jets_eta_0', 'gen_jets_eta_1', 'gen_jets_eta_2', 'gen_jets_eta_3', 'gen_jets_eta_4', 'gen_jets_eta_5', 'gen_jets_eta_6', 'gen_jets_eta_7', 'gen_jets_eta_8', 'gen_jets_eta_9', 'gen_jets_phi_0', 'gen_jets_phi_1', 'gen_jets_phi_2', 'gen_jets_phi_3', 'gen_jets_phi_4', 'gen_jets_phi_5', 'gen_jets_phi_6', 'gen_jets_phi_7', 'gen_jets_phi_8',
+'gen_jets_phi_9', 'gen_jets_mass_0', 'gen_jets_mass_1', 'gen_jets_mass_2', 'gen_jets_mass_3', 'gen_jets_mass_4', 'gen_jets_mass_5', 'gen_jets_mass_6', 'gen_jets_mass_7', 'gen_jets_mass_8', 'gen_jets_mass_9', 'gen_jets_matchFlag_0', 'gen_jets_matchFlag_1', 'gen_jets_matchFlag_2', 'gen_jets_matchFlag_3', 'gen_jets_matchFlag_4', 'gen_jets_matchFlag_5', 'gen_jets_matchFlag_6', 'gen_jets_matchFlag_7', 'gen_jets_matchFlag_8', 'gen_jets_matchFlag_9', 'gen_num_nu', 'gen_nu_pt_0', 'gen_nu_pt_1',
+'gen_nu_eta_0', 'gen_nu_eta_1', 'gen_nu_phi_0', 'gen_nu_phi_1']
+parton_cols = ['top_pt', 'top_eta', 'top_phi', 'top_mass', 'atop_pt', 'atop_eta', 'atop_phi', 'atop_mass', 'bottom_pt', 'bottom_eta', 'bottom_phi', 'bottom_mass', 'abottom_pt', 'abottom_eta', 'abottom_phi', 'abottom_mass']
+target_cols = ['prob_ttH', 'prob_ttbb', 'JLR']
+
+#['jets_matchFlag_0', 'jets_matchFlag_1', 'jets_matchFlag_2', 'jets_matchFlag_3', 'jets_matchFlag_4', 'jets_matchFlag_5', 'jets_matchFlag_6', 'jets_matchFlag_7', 'jets_matchFlag_8', 'jets_matchFlag_9',]
+#['nMatch_wq', 'nMatch_tb', 'nMatch_hb',]
+
+
+for fn in glob.glob("data/Jun5/*.csv".format(args.type))[:args.maxfiles]:
     data = pd.read_csv(fn, delim_whitespace=True)
     cols = list(data.columns)
-    ic1 = cols.index("gen_num_leptons")
-    ic2 = cols.index("top_pt")
-    reco_cols = cols[:ic1]
-    gen_cols = cols[ic1:ic2]
-    parton_cols = cols[ic2:-3]
 
     print "precut", data.shape
     if len(args.cut) > 0:
@@ -43,13 +53,10 @@ for fn in glob.glob("samples/combined/*.csv".format(args.type))[:args.maxfiles]:
     print "postcut", data.shape
     if args.type == "parton":
         feature_cols = parton_cols
-        target_cols = cols[-3:]
     elif args.type == "reco":
         feature_cols = reco_cols
-        target_cols = cols[-3:]
     elif args.type == "gen":
         feature_cols = gen_cols
-        target_cols = cols[-3:]
     print feature_cols
     print target_cols
     X = data[feature_cols].as_matrix().astype("float32")
